2dbfca9774b26390aff5147950f0b07923768fca
Peerapong Maitriwong <dev.peerapong@gmail.com>
2018-01-03 15:31:19 +0700

    added navigation

---
+++ packages/react-scripts/template/src/layouts/AppLayout.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { NavLink } from 'react-router-dom'
+import {
+  RouteConfig,
+  renderRoutes,
+  RouteConfigComponentProps
+} from 'react-router-config'
+
+import '../styles/layouts/AppLayout.css'
+
+export interface AppLayoutProps
+  extends RouteConfigComponentProps<typeof AppLayout> {
+  routes: RouteConfig[]
+}
+
+function AppLayout(props: AppLayoutProps) {
+  return (
+    <section className="app-layout">
+      <main className="main">{renderRoutes(props.route!.routes)}</main>
+      <nav className="navigation">
+        <NavLink className="link" to="/" exact={true}>
+          <i className="ion-ios-home" /> Home
+        </NavLink>
+        <NavLink className="link" to="/counters">
+          <i className="ion-ios-speedometer" />
+          Counters
+        </NavLink>
+      </nav>
+    </section>
+  )
+}
+
+export default AppLayout
--- packages/react-scripts/template/src/routes/index.ts
+++ packages/react-scripts/template/src/routes/index.ts
@@ -3,8 +3,11 @@ import { RouteConfig } from 'react-router-config'
 import CounterRoutes from './counters'
 
 import App from './default/screens/App'
+import AppLayout from '../layouts/AppLayout'
 
 export default [
-  { path: '/', component: App, exact: true },
-  ...CounterRoutes
+  {
+    component: AppLayout,
+    routes: [{ path: '/', component: App, exact: true }, ...CounterRoutes]
+  }
 ] as RouteConfig[]
