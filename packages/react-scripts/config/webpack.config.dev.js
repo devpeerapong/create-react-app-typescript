c45279532380f1f4fa1717929cdda3b153743f2c
Peerapong Maitriwong <dev.peerapong@gmail.com>
2018-02-06 13:31:10 +0700

    add postcss-next

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -194,15 +194,6 @@ module.exports = {
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
                     require('postcss-cssnext'),
                   ],
                 },
